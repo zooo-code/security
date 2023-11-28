d277d82db6b6c2d87d804d90b59e34f502355974
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-11-28 21:30:43 +0900

    study

--- src/main/java/com/security/user/controller/UserViewController.java
+++ src/main/java/com/security/user/controller/UserViewController.java
@@ -1,6 +1,9 @@
 package com.security.user.controller;
 
+import com.security.user.entity.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,7 +13,11 @@ public class UserViewController {
     public String home() {
         return "home";
     }
-
+    @GetMapping("/home")
+    public String home(Model model, @AuthenticationPrincipal User userInfo) throws Exception {
+        model.addAttribute("userInfo", userInfo);
+        return "home";
+    }
     @GetMapping("/login")
     public String login() {
         return "login";
